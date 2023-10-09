8ab30a34e1f56ad06c50fc077fd5bcdbfb3d0f4e
MagUlitka <magdalena.cyra9@gmail.com>
2023-10-09 16:54:28 +0200

    Project initialization

---
+++ src/main/java/com/example/mprprojectmvn/MprProjectMvnApplication.java
@@ -0,0 +1,13 @@
+package com.example.mprprojectmvn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MprProjectMvnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MprProjectMvnApplication.class, args);
+	}
+
+}
